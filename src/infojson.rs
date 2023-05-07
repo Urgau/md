2ebb4df190ce189666cd13830a327dc671c5af0f
Urgau <urgau@numericable.fr>
2023-05-07 19:29:40 +0200

    MVP

---
+++ src/infojson.rs
@@ -0,0 +1,187 @@
+//! InfoJson models
+
+use serde::{Deserialize, Deserializer, Serialize};
+use std::{collections::HashMap, marker::PhantomData, str::FromStr};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct InfoJson {
+    pub id: String,
+    pub title: String,
+    pub formats: Vec<Format>,
+    pub thumbnails: Option<Vec<Thumbnail>>,
+    pub thumbnail: Option<String>,
+    pub description: Option<String>,
+    pub uploader: Option<String>,
+    pub uploader_id: Option<String>,
+    pub uploader_url: Option<String>,
+    pub channel_id: Option<String>,
+    pub channel_url: Option<String>,
+    pub duration: i64,
+    pub view_count: Option<i64>,
+    pub age_limit: Option<i64>,
+    pub webpage_url: String,
+    pub categories: Option<Vec<String>>,
+    pub tags: Option<Vec<String>>,
+    // pub playable_in_embed: Option<bool>,
+    // pub live_status: Option<String>,
+    pub automatic_captions: Option<HashMap<String, Vec<AutomaticCaption>>>,
+    // pub subtitles: Subtitles,
+    pub comment_count: Option<i64>,
+    pub like_count: Option<i64>,
+    pub channel: Option<String>,
+    pub channel_follower_count: Option<i64>,
+    pub upload_date: Option<String>,
+    pub availability: Option<String>,
+    // pub webpage_url_basename: String,
+    // pub webpage_url_domain: String,
+    pub extractor: String,
+    pub extractor_key: String,
+    pub display_id: String,
+    pub fulltitle: String,
+    pub duration_string: String,
+    pub is_live: Option<bool>,
+    pub was_live: Option<bool>,
+    pub format: String,
+    pub format_id: String,
+    pub ext: String,
+    pub protocol: String,
+    pub format_note: Option<String>,
+    pub filesize_approx: i64,
+    pub tbr: f64,
+    pub width: i64,
+    pub height: i64,
+    pub resolution: String,
+    pub fps: Option<f64>,
+    pub dynamic_range: Option<String>,
+    #[serde(deserialize_with = "lit_none_string")]
+    pub vcodec: Option<String>,
+    pub vbr: f64,
+    pub aspect_ratio: Option<f64>,
+    #[serde(deserialize_with = "lit_none_string")]
+    pub acodec: Option<String>,
+    pub abr: Option<f64>,
+    pub asr: Option<i64>,
+    pub audio_channels: Option<i64>,
+    pub epoch: i64,
+    #[serde(rename = "_type")]
+    pub info_json_type: String,
+    #[serde(rename = "_version")]
+    pub version: Version,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AutomaticCaption {
+    pub ext: String,
+    pub url: String,
+    pub name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Format {
+    pub format_id: String,
+    pub format_note: Option<String>,
+    pub ext: String,
+    pub protocol: String,
+    #[serde(deserialize_with = "lit_none_string")]
+    pub acodec: Option<String>,
+    #[serde(deserialize_with = "lit_none_string")]
+    pub vcodec: Option<String>,
+    pub url: String,
+    pub width: Option<i64>,
+    pub height: Option<i64>,
+    pub fps: Option<f64>,
+    pub rows: Option<i64>,
+    pub columns: Option<i64>,
+    pub fragments: Option<Vec<Fragment>>,
+    #[serde(deserialize_with = "lit_none_string")]
+    pub resolution: Option<String>,
+    pub aspect_ratio: Option<f64>,
+    pub http_headers: HttpHeaders,
+    pub audio_ext: String,
+    pub video_ext: String,
+    pub format: String,
+    pub asr: Option<i64>,
+    pub filesize: Option<u64>,
+    pub source_preference: Option<i64>,
+    pub audio_channels: Option<i64>,
+    pub quality: Option<f64>,
+    pub has_drm: Option<bool>,
+    pub tbr: Option<f64>,
+    pub language_preference: Option<i64>,
+    pub abr: Option<f64>,
+    pub container: Option<String>,
+    pub preference: Option<i64>,
+    pub dynamic_range: Option<String>,
+    pub vbr: Option<f64>,
+    pub filesize_approx: Option<i64>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Fragment {
+    pub url: String,
+    pub duration: Option<f64>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct HttpHeaders {
+    #[serde(rename = "User-Agent")]
+    pub user_agent: String,
+    #[serde(rename = "Accept")]
+    pub accept: String,
+    #[serde(rename = "Accept-Language")]
+    pub accept_language: String,
+    #[serde(rename = "Sec-Fetch-Mode")]
+    pub sec_fetch_mode: String,
+}
+
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct Subtitles {
+// }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Thumbnail {
+    pub url: String,
+    pub preference: i64,
+    pub id: String,
+    pub height: Option<i64>,
+    pub width: Option<i64>,
+    pub resolution: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Version {
+    pub version: String,
+    pub release_git_head: String,
+    pub repository: String,
+}
+
+fn lit_none_string<'de, T, D>(deserializer: D) -> Result<Option<T>, D::Error>
+where
+    T: Deserialize<'de> + FromStr<Err = std::convert::Infallible>,
+    D: Deserializer<'de>,
+{
+    struct LitNoneString<T>(PhantomData<fn() -> T>);
+
+    impl<'de, T> serde::de::Visitor<'de> for LitNoneString<T>
+    where
+        T: Deserialize<'de> + FromStr<Err = core::convert::Infallible>,
+    {
+        type Value = Option<T>;
+
+        fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+            formatter.write_str("string")
+        }
+
+        fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: serde::de::Error,
+        {
+            match v {
+                "none" => Ok(None),
+                _ => Ok(Some(FromStr::from_str(v).unwrap())),
+            }
+        }
+    }
+
+    deserializer.deserialize_any(LitNoneString(PhantomData))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,220 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fmt::Display, fs::File, io::BufReader, process::Command};
+
+use anyhow::{bail, Context};
+use clap::Parser;
+use humansize::{SizeFormatter, BINARY};
+use inquire::{Select};
+use tempfile::TempDir;
+
+mod infojson;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Verbosity
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+
+    /// Url of the media to download
+    url: String,
+
+    /// Extra arguments to pass to yt-dlp
+    #[arg(last = true)]
+    extras: Vec<String>,
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let args = Args::parse();
+
+    let tempdir = std::mem::ManuallyDrop::new(
+        TempDir::new().context("couldn't create the temporary directory")?,
+    );
+
+    let mut command = Command::new("yt-dlp");
+
+    command
+        .arg("--write-info-json")
+        .arg("--skip-download")
+        .arg("--no-playlist")
+        .arg("-P")
+        .arg(tempdir.path())
+        .arg(&args.url)
+        .args(&args.extras);
+
+    let status = command.status()?;
+
+    if !status.success() {
+        bail!("yt-dlp error: {:?}", command);
+    }
+
+    let info_json_entry = std::fs::read_dir(tempdir.path())
+        .with_context(|| tempdir.path().display().to_string())?
+        .filter_map(|entry| {
+            if let Ok(entry) = entry {
+                if entry.file_type().ok().map_or(false, |ft| ft.is_file()) {
+                    Some(entry)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .next()
+        .context("directory empty")?;
+
+    let info_json =
+        BufReader::new(File::open(info_json_entry.path()).with_context(|| {
+            format!("unable to open file: {}", info_json_entry.path().display())
+        })?);
+    let info_json: infojson::InfoJson = serde_json::from_reader(info_json).with_context(|| {
+        format!(
+            "unable to read the info_json file: {}",
+            info_json_entry.path().display()
+        )
+    })?;
+
+    let mut formats = Vec::new();
+
+    let is_music = info_json.categories.as_ref().map_or(false, |categories| {
+        categories
+            .iter()
+            .any(|cat| cat.eq_ignore_ascii_case("music"))
+    });
+
+    if is_music {
+        match prep_select_audio(info_json.formats.iter()).prompt() {
+            Ok(AudioFormatDisplay(format)) => formats.push(&format.format_id),
+            Err(_) => return Ok(()),
+        }
+        match prep_select_video(info_json.formats.iter()).prompt_skippable() {
+            Ok(Some(VideoFormatDisplay(format))) => formats.push(&format.format_id),
+            Ok(None) => {}
+            Err(_) => return Ok(()),
+        }
+    } else {
+        match prep_select_video(info_json.formats.iter()).prompt() {
+            Ok(VideoFormatDisplay(format)) => formats.push(&format.format_id),
+            Err(_) => return Ok(()),
+        }
+        match prep_select_audio(info_json.formats.iter()).prompt() {
+            Ok(AudioFormatDisplay(format)) => formats.push(&format.format_id),
+            Err(_) => return Ok(()),
+        }
+    }
+
+    let mut command = Command::new("yt-dlp");
+
+    command
+        .arg("--load-info-json")
+        .arg(info_json_entry.path())
+        .arg("--no-playlist")
+        .arg("-f")
+        .arg({
+            let mut ff = String::new();
+
+            ff.push_str(&formats[0]);
+            for f in &formats[1..] {
+                ff.push_str("+");
+                ff.push_str(f);
+            }
+
+            ff
+        })
+        .args(&args.extras);
+
+    let status = command.status()?;
+
+    if !status.success() {
+        bail!("yt-dlp error: {:?}", command);
+    }
+
+    drop(tempdir);
+    Ok(())
+}
+struct AudioFormatDisplay<'a>(&'a infojson::Format);
+
+impl Display for AudioFormatDisplay<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(acodec) = &self.0.acodec {
+            write!(f, "{:.4}", acodec)?;
+        }
+        if let Some(asr) = self.0.asr {
+            f.write_str(" ")?; // todo
+            write!(f, "{}k", asr / 1000)?;
+        }
+        if let Some(filesize) = self.0.filesize {
+            f.write_str(" ")?; // todo
+            write!(f, "{}", SizeFormatter::new(filesize, BINARY))?;
+        }
+        if let Some(format_note) = &self.0.format_note {
+            f.write_str(" ")?; // todo
+            f.write_str(&format_note)?;
+        }
+        Ok(())
+    }
+}
+
+fn prep_select_audio<'a, I: Iterator<Item = &'a infojson::Format>>(
+    formats: I,
+) -> Select<'a, AudioFormatDisplay<'a>> {
+    let options: Vec<AudioFormatDisplay> = formats
+        .filter(|f| f.acodec.is_some() && f.vcodec.is_none())
+        .map(AudioFormatDisplay)
+        .collect();
+
+    Select::new("Which audio format do you want?", options).with_formatter(&|f| {
+        let mut buf = String::new();
+
+        buf.push_str(&f.value.0.format_id);
+        if let Some(acodec) = &f.value.0.acodec {
+            buf.push_str(" - ");
+            buf.push_str(acodec);
+        }
+
+        buf
+    })
+}
+
+struct VideoFormatDisplay<'a>(&'a infojson::Format);
+
+impl Display for VideoFormatDisplay<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if let Some(vcodec) = &self.0.vcodec {
+            write!(f, "{:.4}", vcodec)?;
+        }
+        if let Some(resolution) = &self.0.resolution {
+            write!(f, " {}", resolution)?;
+        }
+        if let Some(filesize) = self.0.filesize {
+            f.write_str(" ")?; // todo
+            write!(f, "{}", SizeFormatter::new(filesize, BINARY))?;
+        }
+        if let Some(format_note) = &self.0.format_note {
+            f.write_str(" ")?; // todo
+            f.write_str(&format_note)?;
+        }
+        Ok(())
+    }
+}
+
+fn prep_select_video<'a, I: Iterator<Item = &'a infojson::Format>>(
+    formats: I,
+) -> Select<'a, VideoFormatDisplay<'a>> {
+    let options: Vec<VideoFormatDisplay> = formats
+        .filter(|f| f.vcodec.is_some() && f.acodec.is_none())
+        .map(VideoFormatDisplay)
+        .collect();
+
+    Select::new("Which video format do you want?", options).with_formatter(&|f| {
+        let mut buf = String::new();
+
+        buf.push_str(&f.value.0.format_id);
+        if let Some(vcodec) = &f.value.0.vcodec {
+            buf.push_str(" - ");
+            buf.push_str(vcodec);
+        }
+
+        buf
+    })
 }
